17dacf59502dfea02ce5d98fddce459b75891e17
Rafael Calcada <rafaelcalcada@gmail.com>
2024-09-29 18:13:21 +0200

    Add libsteel.h to include dir

---
+++ libsteel/libsteel.h
@@ -0,0 +1,17 @@
+// ----------------------------------------------------------------------------
+// Copyright (c) 2020-2024 RISC-V Steel contributors
+//
+// This work is licensed under the MIT License, see LICENSE file for details.
+// SPDX-License-Identifier: MIT
+// ----------------------------------------------------------------------------
+
+#ifndef __RVSTEEL_LIBSTEEL__
+#define __RVSTEEL_LIBSTEEL__
+
+#include "csr.h"
+#include "gpio.h"
+#include "mtimer.h"
+#include "spi.h"
+#include "uart.h"
+
+#endif // __RVSTEEL_LIBSTEEL__
